959bf20e9c105a48d46913f97d3cc2bc969f34f7
adamjogkins-eng <adamjogkins@gmail.com>
2026-01-18 20:07:35 +0000

    Update Exec.mm

--- Exec.mm
+++ Exec.mm
@@ -1,6 +1,5 @@
 #import <UIKit/UIKit.h>
 
-// Bridge to the logic you extracted from Delta
 extern "C" void execute_lua(const char* script);
 
 @interface BlockzUI : UIView
@@ -15,7 +14,7 @@ extern "C" void execute_lua(const char* script);
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        // 1. Floating '𝔅' Anchor
+        // Floating '𝔅' Anchor
         self.anchorBtn = [UIButton buttonWithType:UIButtonTypeCustom];
         self.anchorBtn.frame = CGRectMake(40, 60, 55, 55);
         self.anchorBtn.backgroundColor = [UIColor colorWithRed:0.0 green:0.25 blue:0.1 alpha:0.95];
@@ -30,32 +29,32 @@ extern "C" void execute_lua(const char* script);
         [self.anchorBtn addGestureRecognizer:pan];
         [self addSubview:self.anchorBtn];
 
-        // 2. Emerald Menu
+        // Emerald Menu
         self.mainContainer = [[UIView alloc] initWithFrame:CGRectMake(80, 100, 360, 220)];
         self.mainContainer.backgroundColor = [UIColor colorWithRed:0.02 green:0.02 blue:0.02 alpha:0.98];
         self.mainContainer.layer.cornerRadius = 18;
         self.mainContainer.layer.borderColor = [UIColor colorWithRed:0.0 green:0.5 blue:0.2 alpha:1.0].CGColor;
         self.mainContainer.layer.borderWidth = 1.5;
         self.mainContainer.hidden = YES;
+        
+        UIPanGestureRecognizer *panMain = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleMainPan:)];
+        [self.mainContainer addGestureRecognizer:panMain];
         [self addSubview:self.mainContainer];
 
-        // Emerald Header
         UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20, 10, 200, 25)];
         title.text = @"BLOCKZ EXEC V1";
         title.textColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.4 alpha:1.0];
         title.font = [UIFont fontWithName:@"AvenirNext-Bold" size:16];
         [self.mainContainer addSubview:title];
 
-        // Editor
         self.editor = [[UITextView alloc] initWithFrame:CGRectMake(15, 45, 330, 120)];
         self.editor.backgroundColor = [UIColor colorWithRed:0.05 green:0.05 blue:0.05 alpha:1.0];
         self.editor.textColor = [UIColor colorWithRed:0.5 green:1.0 blue:0.7 alpha:1.0];
         self.editor.font = [UIFont fontWithName:@"Menlo" size:11];
         self.editor.layer.cornerRadius = 10;
-        self.editor.text = @"-- BlockzExec Loaded\nprint('Hello Emerald!')";
+        self.editor.text = @"print('BlockzExec Loaded!')";
         [self.mainContainer addSubview:self.editor];
 
-        // Action Buttons
         UIButton *exec = [UIButton buttonWithType:UIButtonTypeSystem];
         exec.frame = CGRectMake(15, 175, 160, 35);
         [exec setTitle:@"EXECUTE" forState:UIControlStateNormal];
@@ -87,6 +86,12 @@ extern "C" void execute_lua(const char* script);
     [p setTranslation:CGPointZero inView:self];
 }
 
+- (void)handleMainPan:(UIPanGestureRecognizer *)p {
+    CGPoint trans = [p translationInView:self];
+    self.mainContainer.center = CGPointMake(self.mainContainer.center.x + trans.x, self.mainContainer.center.y + trans.y);
+    [p setTranslation:CGPointZero inView:self];
+}
+
 - (void)runLua {
     execute_lua([self.editor.text UTF8String]);
 }
@@ -97,9 +102,19 @@ extern "C" void execute_lua(const char* script);
 @end
 
 static void __attribute__((constructor)) load() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(12 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *win = [UIApplication sharedApplication].keyWindow;
-        BlockzUI *ui = [[BlockzUI alloc] initWithFrame:win.bounds];
-        [win addSubview:ui];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *win = nil;
+        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive) {
+                for (UIWindow* window in scene.windows) {
+                    if (window.isKeyWindow) { win = window; break; }
+                }
+            }
+        }
+        if (!win) win = [UIApplication sharedApplication].keyWindow;
+        if (win) {
+            BlockzUI *ui = [[BlockzUI alloc] initWithFrame:win.bounds];
+            [win addSubview:ui];
+        }
     });
 }
